faec96c42dd4d4be12be848e0c75c989681e2bb1
OmarElmorsy <119528435+OmarElmorsy@users.noreply.github.com>
2022-12-18 23:01:29 +0200

    making wallls map

--- src/com/company/BodyGLEventListener.java
+++ src/com/company/BodyGLEventListener.java
@@ -18,226 +18,144 @@ import java.io.InputStream;
 import java.util.ArrayList;
 
 public class BodyGLEventListener implements GLEventListener , KeyListener {
-    double max_X = 1, max_Y = 1, min_X = -1, min_Y = -1;
-    double x = 0, y = 0;
-    double scale = 0.1;
-    int quad_radius=1;
+
     private GLCanvas gcl;
-    double rotate_angle = 0 ;
-    protected String assetsFolderName = "Assets";
+
+    private final int canvasWidth = 100, canvasHeight = 100;
+    private final int wallWidth = 5, wallHeight = 5;
+
+    float x = 50, y = 50;
+    double rotate_angle = 0;
+
+    // Assets Texture
     String textureName[]= {"Tampel2.png","Tank5.png","Shot2.png"};
     TextureReader.Texture texture[] = new TextureReader.Texture[textureName.length] ;
     int textureIndex[] = new int[textureName.length];
-    int index_post = 0 ;
-    int [][] array_of_blocks = new int[20][20];
+    WallMaps walls = new WallMaps();
 
-    ArrayList <ShotMove> Shot_Data = new ArrayList<>();
-    
+    ArrayList <ShotMove> shotData = new ArrayList<>();
 
-    public BodyGLEventListener() throws FileNotFoundException {
-    }
 
     public void setGLCanvas(GLCanvas gcl){
         this.gcl=gcl;
     }
 
-
-
     @Override
     public void init(GLAutoDrawable drawable) {
         GL gl = drawable.getGL();
-        gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);  //This Will Clear The Background Color To Black
+        gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         gl.glViewport(0, 0, 100,100);
         gl.glMatrixMode(GL.GL_PROJECTION);
         gl.glLoadIdentity();
-        gl.glOrtho(0.0, 100.0, 0.0, 100.0, -1.0, 1.0);
-        gl.glEnable(GL.GL_TEXTURE_2D);  // Enable Texture Mapping
+        gl.glOrtho(0.0, (float) canvasWidth, 0.0, (float) canvasHeight, -1.0, 1.0);
+        gl.glEnable(GL.GL_TEXTURE_2D);
         gl.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA);
+
         //number of textures, array to hold the indeces
-        for (int k = 0 ; k < array_of_blocks.length ; k++ ){
-            for (int j = 0 ; j< array_of_blocks.length;j++){
-                array_of_blocks[k][j]= (int)(Math.round(Math.random()));
-            }
-        }
         gl.glGenTextures(textureName.length, textureIndex, 0);
         for (int i = 0 ; i < textureName.length;i++) {
             try {
-                texture [i]= TextureReader.readTexture(assetsFolderName + "//" + textureName[i], true);
+                texture [i]= TextureReader.readTexture("Assets" + "//" + textureName[i], true);
                 gl.glBindTexture(GL.GL_TEXTURE_2D, textureIndex[i]);
-                //mipmapsFromPNG(gl, new GLU(), texture[i]);
-                new GLU().gluBuild2DMipmaps(
-                        GL.GL_TEXTURE_2D,
-                        GL.GL_RGBA, // Internal Texel Format,
-                        texture[i].getWidth(), texture[i].getHeight(),
-                        GL.GL_RGBA, // External format from image,
-                        GL.GL_UNSIGNED_BYTE,
-                        texture[i].getPixels() // Imagedata
-                );
+                new GLU().gluBuild2DMipmaps(GL.GL_TEXTURE_2D, GL.GL_RGBA, texture[i].getWidth(), texture[i].getHeight(), GL.GL_RGBA, GL.GL_UNSIGNED_BYTE, texture[i].getPixels());
             } catch (IOException e) {
                 System.out.println(e);
                 e.printStackTrace();
             }
         }
+
     }
 
     @Override
     public void display(GLAutoDrawable drawable) {
+        int index_post = 0;
         GL gl = drawable.getGL();
         gl.glClear (GL.GL_COLOR_BUFFER_BIT);
-        for (int i = 0 ; i < array_of_blocks.length ; i++ ){
-            for (int j = 0 ; j< array_of_blocks.length;j++){
-                if (array_of_blocks[i][j]==1){
-                    DrawSprite(gl,index_post,0,(float)(i*5),(float) (j*5));
+        for (int i = 0 ; i < walls.map.length; i++ ){
+            for (int j = 0 ; j< walls.map[i].length;j++){
+                if (walls.map[walls.map.length - i -1 ][j]==1){
+                    drawTexture(gl,index_post,0, (float)(j*wallWidth),(float) (i*wallHeight), (float)wallWidth, (float)wallHeight);
                 }
             }
         }
+//        drawTexture(gl,2,0, (float)(7*wallWidth),(float) (7*wallHeight), (float)1, (float)2);
+        drawTexture(gl,1, rotate_angle, x , y, 5, 5);
 
-        for (int i = 0 ; i < Shot_Data.size() ; i ++){
-            Shot(gl,Shot_Data.get(i).x,Shot_Data.get(i).y,Shot_Data.get(i).rotate_angle);
+        for (int i = 0; i < shotData.size() ; i ++){
+            drawTexture(gl,index_post, shotData.get(i).rotate_angle, shotData.get(i).x, shotData.get(i).y, wallWidth, wallHeight);
 
-            if (Shot_Data.get(i).rotate_angle==0){
-                Shot_Data.get(i).y+=.2;
-            }else if (Shot_Data.get(i).rotate_angle==90){
-                Shot_Data.get(i).x-=.2;
-            }else if (Shot_Data.get(i).rotate_angle==180){
-                Shot_Data.get(i).y-=.2;
-            }else if (Shot_Data.get(i).rotate_angle==-90){
-                Shot_Data.get(i).x+=.2;
-            }else if(Shot_Data.get(i).y > 8||Shot_Data.get(i).x>8||Shot_Data.get(i).y<-8||Shot_Data.get(i).x<-8)
+            if (shotData.get(i).rotate_angle==0){
+                shotData.get(i).y+=.2;
+            }else if (shotData.get(i).rotate_angle==90){
+                shotData.get(i).x-=.2;
+            }else if (shotData.get(i).rotate_angle==180){
+                shotData.get(i).y-=.2;
+            }else if (shotData.get(i).rotate_angle==-90){
+                shotData.get(i).x+=.2;
+            }else if(shotData.get(i).y > 8|| shotData.get(i).x>8|| shotData.get(i).y<-8|| shotData.get(i).x<-8)
             {
-                Shot_Data.remove(i);
+                shotData.remove(i);
                }
 
         }
 
     }
 
-    @Override
-    public void reshape(GLAutoDrawable drawable, int i, int i1, int i2, int i3) {
-
-    }
-
-    @Override
-    public void displayChanged(GLAutoDrawable drawable, boolean b, boolean b1) {
-
-    }
-    public void DrawSprite(GL gl ,int index, double rotate_angle, float xLeft, float yTop) {
+    public void drawTexture(GL gl , int index, double rotate_angle,float xLeft, float yBottom, float Width,float Height) {
         gl.glEnable(GL.GL_BLEND);
-        gl.glBindTexture(GL.GL_TEXTURE_2D, textureIndex[0]);	// Turn Blending On
-        //gl.glColor3f(0, 0, 0);
+        gl.glBindTexture(GL.GL_TEXTURE_2D, textureIndex[index]);	// Turn Blending On
         gl.glPushMatrix();
-        gl.glTranslated( x * scale, y * scale, 1);
-        gl.glRotated(rotate_angle,0,0,1);
-        //gl.glScaled(scale*2, scale*3.5, 1);
-        //System.out.println(x +" " + y);
+        if(index == 1){
+            gl.glTranslated(x*.1, y*0.01,1);
+            gl.glRotated((float)rotate_angle,0,0,1);
+        }
         gl.glBegin(GL.GL_QUADS);
-        // Front Face
         gl.glTexCoord2f(0.0f, 0.0f);
-        gl.glVertex3f(xLeft, yTop, -1.0f);
+        gl.glVertex3f(xLeft, yBottom, -1.0f);
         gl.glTexCoord2f(1.0f, 0.0f);
-        gl.glVertex3f(xLeft+5, yTop, -1.0f);
+        gl.glVertex3f(xLeft + Width, yBottom, -1.0f);
         gl.glTexCoord2f(1.0f, 1.0f);
-        gl.glVertex3f(xLeft+5, yTop+5, -1.0f);
+        gl.glVertex3f(xLeft + Width, yBottom + Height, -1.0f);
         gl.glTexCoord2f(0.0f, 1.0f);
-        gl.glVertex3f(xLeft,yTop+5, -1.0f);
-        gl.glEnd();
-        gl.glPopMatrix();
+        gl.glVertex3f(xLeft,yBottom + Height, -1.0f);
 
-        gl.glDisable(GL.GL_BLEND);
-    }
-    public void Shot (GL gl, double x, double y, double rotate_angle) {
-        gl.glEnable(GL.GL_BLEND);
-        gl.glBindTexture(GL.GL_TEXTURE_2D, textureIndex[1]);// Turn Blending On
-        //gl.glColor3f(1.0f, 1.0f, 1.0f);
-        gl.glPushMatrix();
-        gl.glTranslated( x * scale, y * scale+.02, 1);
-        gl.glRotated(rotate_angle, 0, 0, 1);
-        gl.glScaled(scale*.1, scale-.05, 1);
-        //System.out.println(x +" " + y);
-        gl.glBegin(GL.GL_QUADS);
-        // Front Face
-        gl.glTexCoord2f(0.0f, 0.0f);
-        gl.glVertex3f(-1.0f, -1.0f, -1.0f);
-        gl.glTexCoord2f(1.0f, 0.0f);
-        gl.glVertex3f(1.0f, -1.0f, -1.0f);
-        gl.glTexCoord2f(1.0f, 1.0f);
-        gl.glVertex3f(1.0f, 1.0f, -1.0f);
-        gl.glTexCoord2f(0.0f, 1.0f);
-        gl.glVertex3f(-1.0f, 1.0f, -1.0f);
         gl.glEnd();
         gl.glPopMatrix();
         gl.glDisable(GL.GL_BLEND);
-    }
-
-    void DrawBackground (GL gl){
-
-        gl.glEnable(GL.GL_BLEND);	// Turn Blending On
-        //  gl.glBindTexture(GL.GL_TEXTURE_2D, textureIndex[1]);
-        gl.glPushMatrix();
-        gl.glColor3f(0f,0.5f,0.5f);
-        //gl.glScaled(0.5, 0.1, 1);
-        gl.glBegin(GL.GL_QUADS);
-        // Front Face
-
-        gl.glTexCoord2f(0.0f, 0.0f);
-        gl.glVertex3f(-1f, -1f, -1.0f);
-        gl.glTexCoord2f(1.0f, 0.0f);
-        gl.glVertex3f(1.0f, -1.0f, -1.0f);
-        gl.glTexCoord2f(1.0f, 1.0f);
-        gl.glVertex3f(1.0f, 1.0f, -1.0f);
-        gl.glTexCoord2f(0.0f, 1.0f);
-        gl.glVertex3f(-1.0f, 1.0f, -1.0f);
-        gl.glEnd();
-        gl.glPopMatrix();
-        gl.glDisable(GL.GL_BLEND);
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-
+        double scale = 0.1;
+        double max_X = 1, max_Y = 1, min_X = -1, min_Y = -1;
+        int quad_radius=1;
         if (e.getKeyCode()==KeyEvent.VK_UP){
-            if ((y + quad_radius) * scale < max_Y) {
-                y += scale * 2;
-
-            }
+            y += scale * 2;
             rotate_angle=0;
         }
         if (e.getKeyCode()==KeyEvent.VK_DOWN){
-            if ((y - quad_radius) * scale > min_Y) {
                 y -= scale * 2;
-
-            }
-            rotate_angle=180;
+                rotate_angle=180;
         }
         if (e.getKeyCode()==KeyEvent.VK_LEFT){
-            if ((x - quad_radius) * scale > min_X) {
-                x -= scale * 2;
-
-            }
+            x -= scale * 2;
             rotate_angle=90;
         }
         if (e.getKeyCode()==KeyEvent.VK_RIGHT){
-            if ((x + quad_radius) * scale < max_X) {
                 x += scale * 2;
+                rotate_angle=-90;
 
-            }
-            rotate_angle=-90;
         }
         if (e.getKeyCode()==KeyEvent.VK_SPACE){
-            Shot_Data.add(new ShotMove(x,y,rotate_angle));
+            shotData.add(new ShotMove(x,y,rotate_angle));
             // --------------- Call Method Sound_start to make sound shot ------------------//
-            Sound_start();
+            soundBegin();
 
         }
     
     }
 
-    private void Sound_start() {
+    private void soundBegin() {
          InputStream Shot_sound;
          AudioStream Shot_start = null;
         
@@ -253,7 +171,24 @@ public class BodyGLEventListener implements GLEventListener , KeyListener {
     }
 
     @Override
+    public void reshape(GLAutoDrawable drawable, int i, int i1, int i2, int i3) {
+
+    }
+
+    @Override
+    public void displayChanged(GLAutoDrawable drawable, boolean b, boolean b1) {
+    }
+
+    @Override
     public void keyReleased(KeyEvent e) {
 
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    public BodyGLEventListener() throws FileNotFoundException {
     }
 }
