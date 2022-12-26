7768096d0fb75cdb30608e80aa29248c4d1f095c
OmarElmorsy <119528435+OmarElmorsy@users.noreply.github.com>
2022-12-26 21:11:51 +0200

    add update

--- src/com/company/ShotMove.java
+++ src/com/company/ShotMove.java
@@ -5,11 +5,12 @@ public class ShotMove {
     float heightShot = 2;
     float xShot = 0, yShot = 0;
     int direction = 0 , indexTexture = 0 ;
+    boolean isEnemyShot = false ;
 
 
 
 
-    ShotMove (float x, float y, int direction , boolean isEnemy) {
+    ShotMove (float x, float y, int direction, boolean isEnemyShot) {
 
         this.direction = direction;
         this.xShot = (float) (x);
